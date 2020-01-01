b51a5d95fc8165e66542aa65a63502a8d2b5c82b
Devendra Singh <devsinghindra@gmail.com>
2020-01-01 21:26:59 +0530

    added constant file

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Colors.black;
+const Color kSecondaryColor = Colors.deepOrangeAccent;
+const Color kAccentColor=Colors.black54;
+const Color kTextColor=Colors.white70;
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,12 +7,52 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Card(
-              color: Colors.yellow,
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text('Library Books'),
+                      ],
+                    ),
+                  ),
+                  color: Colors.yellow,
+                ),
+              ),
+              Expanded(
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: <Widget>[
+                        Text('Personel Books'),
+                      ],
+                    ),
+                  ),
+                  color: Colors.yellow,
+                ),
+              ),
+              Expanded(
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: <Widget>[
+                        Text('Search from the Library'),
+                      ],
+                    ),
+                  ),
+                  color: Colors.yellow,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
