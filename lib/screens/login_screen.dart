bf40d67754e47185538a30baeded8e0f1ba101f2
Devendra Singh <devsinghindra@gmail.com>
2020-01-03 01:29:42 +0530

    added login screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  static const id = "login_screen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
