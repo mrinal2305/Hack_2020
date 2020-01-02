3c78ea674baadd9ab7ecbfc5ffb7ad1b12682b34
Devendra Singh <devsinghindra@gmail.com>
2020-01-03 01:38:06 +0530

    added home screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const id = 'home_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
