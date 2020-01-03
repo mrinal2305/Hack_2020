40bc2cb91d910bb8d28360e9efc75cbbe52d5374
Devendra Singh <devsinghindra@gmail.com>
2020-01-03 17:39:00 +0530

    added card for different function

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,33 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: <Widget>[
+            MyCard('Add\nBook'),
+            MyCard('Issue\nReissue\nReturn'),
+            MyCard('Search'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class MyCard extends StatelessWidget {
+  final String title;
+  MyCard(this.title);
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: Container(
+          height: 180,
+            width: 350,
+            child: Text(title),
+        ),
+      color: Colors.blue,
+    );
   }
 }
