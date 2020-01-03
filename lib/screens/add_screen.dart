c2ddaa37904682904e0f75bd662b6e981f9a5eb7
Devendra Singh <devsinghindra@gmail.com>
2020-01-03 17:54:07 +0530

    added add book screen

---
+++ lib/screens/add_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class AddScreen extends StatelessWidget {
+  static const id = 'add_screen';
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.red,
+          child: Icon(
+            Icons.add
+          ),
+        ),
+        body: ListView(
+        children: <Widget>[
+          MyCard('Think and grow rich',(){
+
+          }),
+          MyCard('Think and grow rich',(){
+
+          }),
+          MyCard('Think and grow rich',(){
+
+          })
+        ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyCard extends StatelessWidget {
+  final String title;
+  final Function onTap;
+
+  MyCard(this.title, this.onTap);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        child: Container(
+          height: 180,
+          width: 350,
+          child: Text(title),
+        ),
+        color: Colors.blue,
+      ),
+    );
+  }
+}
