c0b90009fcfeef5cc4a19db670e62400b1f8727c
Mrinal Kumar Karn <mrinalkumarkarn24@gmail.com>
2020-01-10 02:52:01 +0530

    added firebase config code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -9,6 +12,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features
     BrowserModule,
     AppRoutingModule
   ],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+   firebase : {
+    apiKey: "AIzaSyAY5_RxZZJEp9f8DX188XMobnEgF-HSWGk",
+    authDomain: "smart-library-55e4d.firebaseapp.com",
+    databaseURL: "https://smart-library-55e4d.firebaseio.com",
+    projectId: "smart-library-55e4d",
+    storageBucket: "smart-library-55e4d.appspot.com",
+    messagingSenderId: "1003411059082",
+    appId: "1:1003411059082:web:36a442c68a793012761873",
+    measurementId: "G-PL3CX9Q6N9"
+  },
   production: false
 };
 
