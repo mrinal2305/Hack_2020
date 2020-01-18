817a48d6e62f1bca9a3387ccc19cd5473ffd57eb
Mrinal Kumar Karn <mrinalkumarkarn24@gmail.com>
2020-01-18 08:27:02 +0530

    Allowed CORS

--- server.js
+++ server.js
@@ -7,6 +7,21 @@ const emotionRoutes = require('./emotion');
 const sentimentRoutes = require('./sentiment');
 const subCategRoutes  = require('./subCategories');
 
+// Allowing CORS 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+  });
+
+
 app.get('/',(req,res,next)=>{
     res.send({
         message : "Add valid url"
