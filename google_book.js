6004232a6a7341abcbd12dc7d355a25cf9601d5c
Mrinal Kumar Karn <mrinalkumarkarn24@gmail.com>
2020-01-18 08:23:34 +0530

    Allowed CORS

--- google_book.js
+++ google_book.js
@@ -4,6 +4,20 @@ var port    = process.env.PORT || 3000;
 var convert  = require('xml-js');
 const app   = express();
 
+// Allowing CORS 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+  });
+
 app.get('/',(req,res,next)=>{
     res.status(200).json({message : "Add title_author or isbn"});
 })
