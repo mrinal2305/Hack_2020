1863597deb0166ee13b87d98f71e745ddbbb1482
Mrinal Kumar Karn <mrinalkumarkarn24@gmail.com>
2020-01-18 16:08:17 +0530

    my work done

--- src/app/book-ser/book-details/book-details.component.ts
+++ src/app/book-ser/book-details/book-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AngularFirestore } from 'angularfire2/firestore';
 
 @Component({
   selector: 'app-book-details',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-details.component.css']
 })
 export class BookDetailsComponent implements OnInit {
-
-  constructor() { }
+  id;
+  data;
+  constructor(public route : ActivatedRoute,public db:AngularFirestore) { }
 
   ngOnInit() {
-  }
+    this.id = this.route.snapshot.queryParamMap.get('id');
+    console.log(this.id);
 
+    this.db.collection('book').doc(this.id).get().subscribe(val => {
+      this.data = val.data();
+      console.log(this.data);
+    })
+  }
+  
 }
--- src/app/stu-info/details/details.component.ts
+++ src/app/stu-info/details/details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AngularFirestore } from 'angularfire2/firestore';
+
 
 @Component({
   selector: 'app-details',
@@ -8,13 +10,19 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DetailsComponent implements OnInit {
   id : any;
-  constructor(public route : ActivatedRoute) { }
+  data ;
+  constructor(public route : ActivatedRoute,public db:AngularFirestore) { }
   
   ngOnInit() {
 
    this.id = this.route.snapshot.queryParamMap.get('id'); // Getting URL send by previous browser
    console.log(this.id);
    
+   this.db.collection('student').doc(this.id).get().subscribe(x => {
+     this.data = x.data();
+     console.log(this.data);
+   });
+
   }
 
 }
